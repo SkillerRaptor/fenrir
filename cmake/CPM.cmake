410a3b8c34657fb890b3959d8f94b839f8d0a01f
SkillerRaptor <skillerraptor@protonmail.com>
2026-04-18 19:29:19 +0200

    fix: don't download cpm if it already exists

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -16,8 +16,10 @@ endif ()
 
 get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
 
-file(DOWNLOAD
-        https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
-        ${CPM_DOWNLOAD_LOCATION})
+if (NOT EXISTS${CPM_DOWNLOAD_LOCATION})
+    file(DOWNLOAD
+            https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
+            ${CPM_DOWNLOAD_LOCATION})
+endif ()
 
 include(${CPM_DOWNLOAD_LOCATION})
