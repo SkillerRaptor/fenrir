ac271a98cda2739feff7aea8793aa59de2d40074
SkillerRaptor <skillerraptor@protonmail.com>
2026-04-19 10:27:37 +0200

    feat: add format specifiers

--- kernel/include/core/logger.hpp
+++ kernel/include/core/logger.hpp
@@ -10,10 +10,10 @@ namespace logger {
 
 void initialize();
 
-void log(const char *format, ...);
-void info(const char *format, ...);
-void debug(const char *format, ...);
-void warn(const char *format, ...);
-void err(const char *format, ...);
+void log(const char *format, ...) __attribute__((format(printf, 1, 2)));
+void info(const char *format, ...) __attribute__((format(printf, 1, 2)));
+void debug(const char *format, ...) __attribute__((format(printf, 1, 2)));
+void warn(const char *format, ...) __attribute__((format(printf, 1, 2)));
+void err(const char *format, ...) __attribute__((format(printf, 1, 2)));
 
 } // namespace logger
