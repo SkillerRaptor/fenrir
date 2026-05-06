38ab6ffca43a0a687e3fd1040c820a30d0a96cdd
SkillerRaptor <skillerraptor@protonmail.com>
2026-05-06 13:47:05 +0200

    feat: clean up build root

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,7 @@ set -e
 
 CARGO="$1"
 SOURCE_ROOT="$2"
+BUILD_ROOT="$3"
 
 "$CARGO" clean --manifest-path "$SOURCE_ROOT/Cargo.toml"
 
@@ -11,3 +12,9 @@ rm -rf \
     "$SOURCE_ROOT/third_party" \
     "$SOURCE_ROOT/iso_root" \
     "$SOURCE_ROOT/target"
+
+rm -f \
+    "$BUILD_ROOT/flanterm.stamp" \
+    "$BUILD_ROOT/uacpi.stamp" \
+    "$BUILD_ROOT/limine.stamp" \
+    "$BUILD_ROOT/kernel.stamp"
